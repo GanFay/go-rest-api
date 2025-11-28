a1e9365a8b03f287d651e6ea8eb218a1f925f497
GanFay <macsbiesiedin170109@gmail.com>
2025-11-28 23:31:14 +0200

    +user/delete

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 	http.HandleFunc("/health", handlers.Health)
 	http.HandleFunc("/ping", handlers.Ping)
 	http.HandleFunc("/users", h.Users)
+	http.HandleFunc("/users/delete", h.Delete)
 
 	addr := ":8080"
 	log.Printf("Listening on %s\n", addr)
